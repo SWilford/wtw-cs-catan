3cb8533db2794fa0fc4be28c067bf21f097c0216
Jack Friedman <jackfriedman17@gmail.com>
2022-09-09 14:01:26 -0400

    test

---
+++ Tile.java
@@ -0,0 +1,19 @@
+public class Tile {
+    private String type;
+    private int number;
+
+    public Tile(String t, int n)
+    {
+        type = t;
+        number = n;
+    }
+
+    public String getType() {
+        return type;
+    }
+    public int getNumber()
+    {
+        return number;
+    }
+
+}
