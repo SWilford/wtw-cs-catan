b2658724c96a1eb8af0d64ef7c2b4ba29f057a15
Spencer Wilford <sawilford2006@gmail.com>
2022-09-13 14:52:30 -0400

    Started creating the board

--- src/board.java
+++ src/board.java
@@ -1,4 +1,11 @@
 public class board {
-    private Tile[][] tiles;
+    public board() {
+        Tile[][] tiles = new Tile[5][6];//initializing an array of tiles that represent the board
+        for (Tile[] tile : tiles) {
+            for (int col = 0; col < tile.length; col++) {
+                tile[col] = new Tile("String", 0);
+            }
+        }
+    }
 
 }
