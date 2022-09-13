eec80423444aadc97eef451860da2fc29ac03fcc
Jack Friedman <jackfriedman17@gmail.com>
2022-09-13 11:23:15 -0400

    main class updates idk how to run anything

--- catanDriver.java
+++ catanDriver.java
@@ -1,5 +1,11 @@
 public class catanDriver {
     public static void main(String[] args) {
-
+        JFrame frame = new JFrame("GUI");
+        frame.setSize(750, 750);
+        frame.setLocation(0, 0);
+        CatanGraphics game = new catanGraphics();
+        frame.setContentPane(game);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setVisible(true)
     }
 }
