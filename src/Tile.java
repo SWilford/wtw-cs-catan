aadcf92c172bd7d7ed9ff3bfa60ac1d2035b41d7
Spencer Wilford <sawilford2006@gmail.com>
2022-09-14 11:14:06 -0400

    Most likely finished tile Made parts of the board surrounding the main playing board into frame, harbor and ocean pieces.

--- src/Tile.java
+++ src/Tile.java
@@ -2,6 +2,7 @@ public class Tile {
     private final String type;
     private final int number;
 
+    private boolean specialHarbor;//If a tile is a special harbor or not
     private int nO, noE, soE, sO, soW, noW; //Stores values for shared vertices of hexes
 
     public Tile(String t, int n)//Constructor
@@ -14,15 +15,16 @@ public class Tile {
         sO = 0;
         soW = 0;
         noW = 0; //vertex values defaulted to 0
+        specialHarbor = false; //harbor defaulted to 0
     }
 
     public void setNorth(int nh) {//mutator method for north vertex value
         nO = nh;
-    }
+    }//mutator for north vertex value
 
     public void setNorthEast(int ne) {//mutator method for northeast vertex value
         noE = ne;
-    }
+    }//mutator for northeast vertex value
 
     public void setSouthEast(int se) {//mutator method for southeast vertex value
         soE = se;
@@ -30,14 +32,18 @@ public class Tile {
 
     public void setSouth(int sh) {//mutator method for south vertex value
         sO = sh;
-    }
+    }//mutator for south vertex value
 
     public void getSouthWest(int sw) {//mutator method for southwest vertex value
         soW = sw;
-    }
+    }//mutator for southwest vertex value
 
     public void getNorthWest(int nw) {//mutator method for northwest vertex value
         noW = nw;
+    }//mutator for northwest vertex value
+
+    public void setSpecialHarbor(boolean b) {//mutator for special harbor
+        specialHarbor = b;
     }
 
     public String getType() {
@@ -70,5 +76,9 @@ public class Tile {
 
     public int getNorthWest() { //accessor for northwest vertex
         return noW;
+    }
+
+    public boolean getSpecialHarbor() {
+        return specialHarbor;
     }
 }
