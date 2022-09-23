4fb595a3a008a95b84a321df7367b8509c0aa492
Jack Friedman <jackfriedman17@gmail.com>
2022-09-23 09:53:39 -0400

    just fixing all the warnings

--- src/catanGraphics.java
+++ src/catanGraphics.java
@@ -3,38 +3,29 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class catanGraphics extends JPanel implements MouseListener, MouseMotionListener{
-    private boolean boardDrew;
-    private ImageIcon titlePic = new ImageIcon("images/title.png");
-    private ImageIcon startBack = new ImageIcon("images/background.gif");
-    private ImageIcon rulesButton = new ImageIcon("images/rulesbut.png");
-    private ImageIcon rulesHighlighted = new ImageIcon("images/rulesbutHL.png");
-    private ImageIcon emptyBoard = new ImageIcon("images/blankcatanboard.png");
-    private ImageIcon desertTile = new ImageIcon("images/desertcatantile.png");
-    private ImageIcon mountainTile = new ImageIcon("images/mountainscatantile.png");
-    private ImageIcon fieldTile = new ImageIcon("images/fieldscatantile.png");
-    private ImageIcon forestTile = new ImageIcon("images/forestcatantile.png");
-    private ImageIcon hillTile = new ImageIcon("images/hillscatantile.png");
-    private ImageIcon pastureTile = new ImageIcon("images/pasturecatantile.png");
+    private final ImageIcon titlePic = new ImageIcon("images/title.png");
+    private final ImageIcon startBack = new ImageIcon("images/background.gif");
+    private final ImageIcon emptyBoard = new ImageIcon("images/blankcatanboard.png");
+    private final ImageIcon desertTile = new ImageIcon("images/desertcatantile.png");
+    private final ImageIcon mountainTile = new ImageIcon("images/mountainscatantile.png");
+    private final ImageIcon fieldTile = new ImageIcon("images/fieldscatantile.png");
+    private final ImageIcon forestTile = new ImageIcon("images/forestcatantile.png");
+    private final ImageIcon hillTile = new ImageIcon("images/hillscatantile.png");
+    private final ImageIcon pastureTile = new ImageIcon("images/pasturecatantile.png");
 
     private final board board = new board();
 
     private static final int SIZE = 500;
     private int currentScreen; //home = 1, rules = 2
-    private static final int textSize = 25;
-    private static final int DELAY = 1;
-    private static Timer t;
-    private static int frame;
-    private rulebook rules;
-    private catanButton[] buttons = new catanButton[5];
+
+    private final catanButton[] buttons = new catanButton[5];
     protected static int mouseX;
     protected static int mouseY;
 
-    private String header, ruleText;
     public catanGraphics()
     {
         addMouseListener(this);
         addMouseMotionListener(this);
-        boardDrew = true;
         mouseX = SIZE/2;
         mouseY = SIZE/2;
         Shape rect = new Rectangle(650, SIZE - 100, 75, 50);
@@ -42,6 +33,8 @@ public class catanGraphics extends JPanel implements MouseListener, MouseMotionL
         Shape rect2 = new Rectangle(650, 500, 75, 50);
         Shape rect3 = new Rectangle(500, 300, 150, 100);
         Shape rect4 = new Rectangle(100, 600, 75, 50);
+        ImageIcon rulesButton = new ImageIcon("images/rulesbut.png");
+        ImageIcon rulesHighlighted = new ImageIcon("images/rulesbutHL.png");
         buttons[0] = new catanButton(rect, "rules", rulesHighlighted, rulesButton);
         buttons[1] = new catanButton(rect1, "back", Color.WHITE, Color.GRAY, Color.BLACK);
         buttons[2] = new catanButton(rect2, "settings", Color.YELLOW, Color.RED, Color.BLACK);
@@ -52,6 +45,7 @@ public class catanGraphics extends JPanel implements MouseListener, MouseMotionL
     public void showGame(Graphics g)
     {
         //start screen
+        String header;
         if(currentScreen == 1) {
             g.drawImage(startBack.getImage(), 0,0, 1366, 768, null);
             buttons[0].drawButton(g);
@@ -64,7 +58,7 @@ public class catanGraphics extends JPanel implements MouseListener, MouseMotionL
         else if(currentScreen == 2) //rules screen
         {
             header = "Rules";
-            ruleText = "Catan is a game";
+            String ruleText = "Catan is a game";
             buttons[1].drawButton(g);
             g.drawString(header, 650, 50);
             g.drawString(ruleText, 550, 200);
@@ -109,29 +103,24 @@ public class catanGraphics extends JPanel implements MouseListener, MouseMotionL
             {
                 if(b.getShape().contains(mouseX, mouseY))
                 {
-                    if(b.getTitle().equals("rules"))
-                    {
-                        currentScreen = 2;
-                        repaint();
-                    }
-                    else if(b.getTitle().equals("back"))
-                    {
-                        currentScreen = 1;
-                        repaint();
-                    }
-                    else if(b.getTitle().equals("settings"))
-                    {
-                        currentScreen = 3;
-                        repaint();
-                    }
-                    else if(b.getTitle().equals("Start"))
-                    {
-                        currentScreen = 4;
-                        repaint();
-                    }
-                    else if(b.getTitle().equals("quit"))
-                    {
-                        System.exit(0);
+                    switch (b.getTitle()) {
+                        case "rules" -> {
+                            currentScreen = 2;
+                            repaint();
+                        }
+                        case "back" -> {
+                            currentScreen = 1;
+                            repaint();
+                        }
+                        case "settings" -> {
+                            currentScreen = 3;
+                            repaint();
+                        }
+                        case "Start" -> {
+                            currentScreen = 4;
+                            repaint();
+                        }
+                        case "quit" -> System.exit(0);
                     }
                 }
             }
@@ -157,9 +146,8 @@ public class catanGraphics extends JPanel implements MouseListener, MouseMotionL
 
     public void drawBoard(Graphics g)
     {
-        int x = 500;
+        int x;
         int y = 100;
-        int r;
         g.drawImage(emptyBoard.getImage(), 250, 25, 832, 702, null);
         x = 478;
         for(int c1 = 2; c1 < 5; c1++) {
