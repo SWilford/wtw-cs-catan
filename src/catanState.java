51ca3908c68eb14fab3dd92cb5fd1e6e960a18fe
Spencer Wilford <sawilford2006@gmail.com>
2022-10-03 11:24:35 -0400

    networking

---
+++ src/catanState.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class catanState implements Serializable{
+    public board board = new board();
+    public ArrayList<Player> players = new ArrayList<>();
+    public boolean gameInProgress;
+    public int currentPlayer; //Stores the id of the player whose turn it is
+    public int winner; //The id of the player who won
+
+    public void applyMessage(int sender, Object message) {
+        if(gameInProgress && message instanceof Tile[][] && sender == currentPlayer) { //Updates for changes to buildings and roads
+            Tile[][] move = (Tile[][])message;
+            board.setTiles(move);
+            if(winner()) { //Needs to be called in the other updates as well
+                gameInProgress = false;
+                winner = currentPlayer;
+            }
+        }
+        else if(!gameInProgress && message.equals("newgame")) {
+            startGame();
+        }
+    }
+
+    //Something needs to be created here that will set currentPlayer to the next player in line
+
+    private void startGame() {
+        //Something needs to go here to randomly decide the first player - assigns to currentPlayer
+        winner = -1;
+        gameInProgress = true;
+
+    }
+
+    private boolean winner() {
+        return players.get(currentPlayer).getPoints() >= 10;
+    }
+
+}
