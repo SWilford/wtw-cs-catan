dbb097878e758e252211727b9078122f075d2ee7
Jack Friedman <jackfriedman17@gmail.com>
2022-10-06 10:35:59 -0400

    stupid

--- src/catanGraphics.java
+++ src/catanGraphics.java
@@ -404,14 +404,5 @@ public class catanGraphics extends JPanel implements MouseListener, MouseMotionL
         }
     }
 
-    private static catanGraphics screen;
-    public static void main(String[]args) {
-        screen = new catanGraphics();
-        JFrame frame = new JFrame("catan");    //window title
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setLocation(100, 50);                 //location of game window on the screen
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setContentPane(screen);
-        frame.setVisible(true);
-    }
+
 }
