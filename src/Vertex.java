6939dc4cbe304a7fc7da9adbb83c9abb12e31ab3
Spencer Wilford <sawilford2006@gmail.com>
2022-12-07 11:14:30 -0500

    Start of new vertex/edge system.

---
+++ src/Vertex.java
@@ -0,0 +1,59 @@
+import java.io.Serializable;
+import java.util.*;
+public class Vertex implements Serializable {
+    private final int number;
+    private boolean isBuilt;
+    private String owner;
+    private boolean isCity;
+    private ArrayList<Integer> neighborNumbs = new ArrayList<>();
+    private boolean buildable;
+
+    public Vertex(int number) {
+        this.number = number;
+        isBuilt = false;
+        owner = null;
+        isCity = false;
+        buildable = true;
+    }
+
+    public void addNeighbor(int n) {
+        neighborNumbs.add(n);
+    }
+
+    public void setBuildable() {
+        buildable = false;
+    }
+
+    public void build(String owner) {
+        this.owner = owner;
+        isBuilt = true;
+    }
+
+    public void upgrade() {
+        isCity = true;
+    }
+
+    public ArrayList<Integer> getNeighborNumbs() {
+        return neighborNumbs;
+    }
+
+    public boolean isBuildable() {
+        return buildable;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public boolean isBuilt() {
+        return isBuilt;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public boolean isCity() {
+        return isCity;
+    }
+}
---
+++ src/VertexWeb.java
@@ -0,0 +1,29 @@
+import java.io.Serializable;
+import java.util.*;
+
+public class VertexWeb implements Serializable {
+    private ArrayList<Vertex> vertices = new ArrayList<>();
+    private ArrayList<Edge> edges = new ArrayList<>();
+
+    public VertexWeb() {
+        for(int i = 0; i < 54; i++) {
+            Vertex v = new Vertex(i);
+            vertices.add(v);
+        }
+        //The following should have some way to be optimized, some kind of algorithm.
+        vertices.get(0).addNeighbor(4);
+        vertices.get(0).addNeighbor(5);
+
+        vertices.get(1).addNeighbor(5);
+        vertices.get(1).addNeighbor(6);
+
+        vertices.get(2).addNeighbor(6);
+        vertices.get(2).addNeighbor(7);
+
+        vertices.get(3).addNeighbor(1);
+        vertices.get(3).addNeighbor(8);
+
+        vertices.get().addNeighbor();
+
+    }
+}
